f998fcfa09107a18f23af677f8d3b11dab132240
Guilherme <guilhermed.dev@gmail.com>
2026-02-26 14:12:06 -0300

    refactor(WebApi.Domain.Entities): make minimal changes on BaseEntity class

--- HotelReservation.WebApi/HotelReservation.WebApi.Domain/Entities/BaseEntity.cs
+++ HotelReservation.WebApi/HotelReservation.WebApi.Domain/Entities/BaseEntity.cs
@@ -2,7 +2,9 @@
 
 public class BaseEntity
 {
-  public int Id { get; set; }
+  public int Id { get; private set; }
   public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
   public DateTime UpdatedAt { get; private set; } = DateTime.UtcNow;
+
+  public void OnUpdateEntity() => UpdatedAt = DateTime.UtcNow;
 }

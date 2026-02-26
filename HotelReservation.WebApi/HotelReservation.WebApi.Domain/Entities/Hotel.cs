92bd246d97deac123eb0a1a7d979c6237d422b69
Guilherme <guilhermed.dev@gmail.com>
2026-02-26 14:07:35 -0300

    feat(WebApi): fix hotel entity, add dbcontext for PgSQL and add hotel table migration

--- HotelReservation.WebApi/HotelReservation.WebApi.Domain/Entities/Hotel.cs
+++ HotelReservation.WebApi/HotelReservation.WebApi.Domain/Entities/Hotel.cs
@@ -6,13 +6,21 @@ namespace HotelReservation.WebApi.Domain.Entities;
 public sealed class Hotel : BaseEntity
 {
   public Address Address { get; private set; }
+  public bool Active { get; private set; } = true;
 
-  public ICollection<HotelRoom> Rooms { get; private set; } = [];
+  private Hotel() { }
 
   public Hotel(Address address)
   {
     DomainException.ThrowsWhen((address == null, "Address must be provided"));
 
+    Address = address!;
+  }
+
+  public void Update(Address address)
+  {
+    DomainException.ThrowsWhen((address == null, "Address must be provided"));
+    
     Address = address!;
   }
 }
---
+++ HotelReservation.WebApi/HotelReservation.WebApi.Infrastructure/Migrations/20260226141751_AddHotelTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HotelReservation.WebApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHotelTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "hotel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Address_Number = table.Column<int>(type: "integer", nullable: false),
+                    Address_Street = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Address_City = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Address_State = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Address_Country = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Address_ZipCode = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamptz", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_hotel", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "hotel");
+        }
+    }
+}
